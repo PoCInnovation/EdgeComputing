1da13e7537b377391b85c65428ccfbbab99e0c14
shellbear <antoine.ordonez@epitech.eu>
2019-04-15 22:21:17 +0200

    Add auto-reload scripts

--- raytracer/webpack.config.js
+++ raytracer/webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.ts',
   resolve: {
-    extensions: ['.ts',]
+    extensions: ['.ts', '.js']
   },
   output: {
     path: path.join(__dirname, '/dist'),
@@ -16,5 +17,11 @@ module.exports = {
         loader: 'ts-loader'
       }
     ]
+  },
+  plugins: [new HtmlWebpackPlugin()],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
   }
 }
